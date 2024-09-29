4dcbf64fa07d5edc8accfa5d0aa2e404a45c7915
Siddharth Saha <siddharth.saha@cred.club>
2024-09-29 11:10:21 +0530

    try tokenizer

---
+++ code/kitchen_sink/tik_tokenize.py
@@ -0,0 +1,25 @@
+"""
+Let's try some tokenization in this script.
+"""
+import tiktoken
+import torch
+
+def main():
+    tokenizer = tiktoken.get_encoding('gpt2')
+
+    text1 = 'Every effort moves you'
+    text2 = 'Every day holds a'
+    text3 = 'Every man speaks up'
+
+    inputs = []
+    inputs.append(torch.tensor(tokenizer.encode(text1)))
+    inputs.append(torch.tensor(tokenizer.encode(text2)))
+    inputs.append(torch.tensor(tokenizer.encode(text3)))
+
+    batch: torch.Tensor = torch.stack(tuple(inputs), dim=0)
+
+    print(batch)
+
+
+if __name__ == '__main__':
+    main()
