b758bccf095950ad9a81a32a6a768001298a894c
Siddharth Saha <siddharth.saha@cred.club>
2024-09-29 11:58:23 +0530

    test layer norm

--- code/layer_norm.py
+++ code/layer_norm.py
@@ -4,6 +4,8 @@ Implements a Layer Normalization class.
 import torch
 from torch import nn
 
+from inputs import BatchGenerator
+
 
 class LayerNorm(nn.Module):
     """
@@ -27,3 +29,28 @@ class LayerNorm(nn.Module):
         norm = (x - mean) / torch.sqrt(var + self.eps)
 
         return self.scale * norm + self.shift
+
+
+def main():
+    """
+    Main method to try out.
+    """
+    batch: torch.Tensor = BatchGenerator.generate()
+    batch = torch.tensor(batch, dtype=torch.float)
+    emb_dim: int = batch.shape[1]
+
+    ln = LayerNorm(emb_dim)
+
+    outputs = ln(batch)
+
+    print(outputs)
+    print(f'{outputs.shape=}')
+
+    mean = torch.mean(outputs, dim=-1, keepdim=True)
+    var = torch.var(outputs, dim=-1, keepdim=True)
+
+    print(mean)
+    print(var)
+
+if __name__ == '__main__':
+    main()
