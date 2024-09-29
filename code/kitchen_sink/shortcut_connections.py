d41b55614ca8a99d918e0371b0a313b2fa9ba322
Siddharth Saha <siddharth.saha@cred.club>
2024-09-29 18:38:14 +0530

    check gradients in shortcut connections

--- code/kitchen_sink/shortcut_connections.py
+++ code/kitchen_sink/shortcut_connections.py
@@ -34,6 +34,20 @@ class ShortcutNN(nn.Module):
             output = m(x)
             x = x + output if self.shortcut and x.shape == output.shape else output
         return x
+    
+def print_gradients(model, inputs, targets):
+    """
+    Creates a NN without shortcut, does a forward and a backward pass, and prints gradients.
+    """
+    outputs = model(inputs)
+    
+    loss = nn.MSELoss()
+    loss = loss(outputs, targets)
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f'{name} has gradient mean of {param.grad.abs().mean().item()}')
 
 
 def main():
@@ -51,13 +65,17 @@ def main():
     inputs = torch.tensor(inputs, dtype=torch.float)
     targets = [0]
     targets = torch.tensor(targets, dtype=torch.float)
+    
     shortcut = False
-
     model = ShortcutNN(layer_sizes, shortcut)
-    outputs = model(inputs)
-    print(outputs)
+    print('=== Without shortcut connections ===')
+    print_gradients(model, inputs, targets)
 
-    print(f'{inputs.shape=}, {targets.shape=}, {outputs.shape=}')
+    shortcut = True
+    model = ShortcutNN(layer_sizes, shortcut)
+    print('=== With shortcut connections === ')
+    print_gradients(model, inputs, targets)
+
 
 
 if __name__ == '__main__':
