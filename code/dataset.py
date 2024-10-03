6fe0280b4fe7faaecf6218fb6773354de13dbaf1
Siddharth Saha <siddharth.saha@cred.club>
2024-10-03 13:09:26 +0530

    fix isort

--- code/dataset.py
+++ code/dataset.py
@@ -3,8 +3,8 @@ This creates the custom dataset to pretrain the LLM.
 """
 import os
 
-from torch.utils.data import Dataset
 import tiktoken
+from torch.utils.data import Dataset
 
 
 class GPTDataset(Dataset):
